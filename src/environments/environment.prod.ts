c1b3a27139078f544c90a9858489d19210f99cfe
mtrybus2208 <mtrybus2208@wp.pl>
2018-05-20 21:31:55 +0200

    Added firebase conf to prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCkgKUqbSlNmJxJykc9VPJA6AfvkNi5SmI',
+    authDomain: 'colour-game-ed095.firebaseapp.com',
+    databaseURL: 'https://colour-game-ed095.firebaseio.com',
+    projectId: 'colour-game-ed095',
+    storageBucket: 'colour-game-ed095.appspot.com',
+    messagingSenderId: '203742208097',
+  }
 };
