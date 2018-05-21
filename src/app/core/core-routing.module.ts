f2e8d5fd12a9bc85cc55c37ccdbafccfbb9b0587
PGS-SOFT\mtrybus <mtrybus@pgs-soft.com>
2018-05-21 15:28:05 +0200

    Remove new-game route

--- src/app/core/core-routing.module.ts
+++ src/app/core/core-routing.module.ts
@@ -3,14 +3,14 @@ import { RouterModule, Routes, PreloadAllModules, PreloadingStrategy, } from '@a
 import { DashboarddGuard } from './../modules/dashboard/quards/dashboard.quard';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'new-game', pathMatch: 'full' },
-  { path: 'new-game', loadChildren: './../modules/new-game/new-game.module#NewGameModule' },
+  // { path: '', redirectTo: 'new-game', pathMatch: 'full' },
+  { path: '', loadChildren: './../modules/new-game/new-game.module#NewGameModule', pathMatch: 'full' },
   { path: 'dashboard',
   // canLoad: [DashboarddGuard],
   loadChildren: './../modules/dashboard/dashboard.module#DashboardModule' },
   { path: 'results', loadChildren: './../modules/results/results.module#ResultsModule' },
   { path: 'best-results', loadChildren: './../modules/best-results/best-results.module#BestResultsModule' },
-  { path: '**', redirectTo: '/new-game', pathMatch: 'full' },
+  { path: '**', redirectTo: '/', pathMatch: 'full' },
 ];
 
 @NgModule({
